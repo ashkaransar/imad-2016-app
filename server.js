6705a9f597f1a038162c3ad4ff2f31ccaf64783e
ashkaransar <ashkaransar6@gmail.com>
2016-09-25 20:56:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/nav_desktops_11151.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'nav_desktops_11151.png'));
 });
 
 
